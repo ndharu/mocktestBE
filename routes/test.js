cefe0869361a06bd7a5800d92d0aeaae66e358a5
ndharu <96077141+ndharu@users.noreply.github.com>
2022-08-05 19:06:38 +0700

    add auth for v2 routes

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var test = require("../controllers/testController");
+const auth = require("../middleware/auth");
 
-router.get("/products", test.test);
+router.get("/products", auth, test.test);
 module.exports = router;
